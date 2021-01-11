f64d8707fbffe849538c9e6dbb60448cd9a40066
Laura-deVere <lauradevh@gmail.com>
2021-01-11 17:59:01 -0500

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// Component
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Configure enzyme for react 16
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  test('should render the app component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.length).toBe(1);
+  })
+})
